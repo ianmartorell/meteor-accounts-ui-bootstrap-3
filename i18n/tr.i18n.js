f95ee0287a1d2baa106ef498fea6c005822df543
Günce BEKTAŞ <guncebektas@gmail.com>
2016-03-18 23:30:49 +0200

    Create tr.i18n.js

---
+++ i18n/tr.i18n.js
@@ -0,0 +1,95 @@
+i18n.map("tr", {
+	resetPasswordDialog: {
+		title: "Şifreyi yenile",
+		newPassword: "Yeni şifre",
+		newPasswordAgain: "Yeni şifre (Tekrar)",
+		cancel: "İptal",
+		submit: "Kaydet"
+	},
+	enrollAccountDialog: {
+		title: "Şifre belirle",
+		newPassword: "Yeni şifre",
+		newPasswordAgain: "Yeni şifre (Tekrar)",
+		cancel: "Kapat",
+		submit: "Kaydet"
+	},
+	justVerifiedEmailDialog: {
+		verified: "Email adresi doğrulandı",
+		dismiss: "Kapat"
+	},
+	loginButtonsMessagesDialog: {
+		dismiss: "Kapat",
+	},
+	loginButtonsLoggedInDropdownActions: {
+		password: "Şifre değiştir",
+		signOut: "Çıkış"
+	},
+	loginButtonsLoggedOutDropdown: {
+		signIn: "Giriş",
+		up: "Kayıt"
+	},
+	loginButtonsLoggedOutPasswordServiceSeparator: {
+		or: "ya da"
+	},
+	loginButtonsLoggedOutPasswordService: {
+		create: "Oluştur",
+		signIn: "Giriş",
+		forgot: "Şifrenizi mi unuttunuz?",
+		createAcc: "Yeni hesap"
+	},
+	forgotPasswordForm: {
+		email: "Email",
+		reset: "Şifre yenile",
+		invalidEmail: "Hatalı email"
+	},
+	loginButtonsBackToLoginLink: {
+		back: "İptal"
+	},
+	loginButtonsChangePassword: {
+		submit: "Şifre değiştir",
+		cancel: "İptal"
+	},
+	loginButtonsLoggedOutSingleLoginButton: {
+		signInWith: "ile giriş yap",
+		configure: "Ayarla",
+	},
+	loginButtonsLoggedInSingleLogoutButton: {
+		signOut: "Çıkış"
+	},
+	loginButtonsLoggedOut: {
+		noLoginServices: "Giriş hizmeti ayarlanmadı"
+	},
+	loginFields: {
+		usernameOrEmail: "Kullanıcıadı ya da email adresi",
+		username: "Kullancıadı",
+		email: "Email",
+		password: "Şifre"
+	},
+	signupFields: {
+		username: "Kullanıcıadı",
+		email: "Email",
+		emailOpt: "Email (Opsiyonel)",
+		password: "Şifre",
+		passwordAgain: "Şifre (Tekrar)"
+	},
+	changePasswordFields: {
+		currentPassword: "Mevcut Şifre",
+		newPassword: "Yeni Şifre",
+		newPasswordAgain: "Yeni Şifre (Tekrar)"
+	},
+	infoMessages : {
+		emailSent: "Email gönderildi",
+		passwordChanged: "Şifre değiştirildi"
+	},
+	errorMessages: {
+		genericTitle: "Bir hata oluştu",
+		userNotFound: "Kullanıcı bulunamadı",
+		invalidEmail: "Hatalı email adresi",
+		incorrectPassword: "Hatalı şifre",
+		usernameTooShort: "Kullanıcıadı en az 3 karakter uzunluğunda olmalıdır",
+		passwordTooShort: "Şifre en az 6 karakter uzunluğunda olmalıdır",
+		passwordsDontMatch: "Şifreler uyuşmuyor",
+		newPasswordSameAsOld: "Yeni şifreniz ile eski şifreniz aynı olmamalıdır",
+		signupsForbidden: "Kayıt olmak engellenmiştir"
+	}
+});
