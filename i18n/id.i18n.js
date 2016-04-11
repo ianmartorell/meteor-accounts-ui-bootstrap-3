20d564b92291276ec3e1d19a97cb4a5bf9806fbc
Agung Yuliaji <agung.yuliaji@gmail.com>
2016-04-11 10:49:00 +0700

    Create id.i18n.js Indonesia Locale

---
+++ i18n/id.i18n.js
@@ -0,0 +1,95 @@
+i18n.map("id", {
+	resetPasswordDialog: {
+		title: "Reset kata sandi anda",
+		newPassword: "Kata sandi baru",
+		newPasswordAgain: "Kata sandi baru (lagi)",
+		cancel: "Batal",
+		submit: "Simpan kata sandi"
+	},
+	enrollAccountDialog: {
+		title: "Isi kata sandi",
+		newPassword: "Kata sandi baru",
+		newPasswordAgain: "Kata sandi baru (again)",
+		cancel: "Close",
+		submit: "Simpan kata sandi"
+	},
+	justVerifiedEmailDialog: {
+		verified: "Email terverifikasi",
+		dismiss: "Tutup"
+	},
+	loginButtonsMessagesDialog: {
+		dismiss: "Tutup",
+	},
+	loginButtonsLoggedInDropdownActions: {
+		password: "Ubah kata sandi",
+		signOut: "Keluar"
+	},
+	loginButtonsLoggedOutDropdown: {
+		signIn: "Masuk",
+		up: "Gabung"
+	},
+	loginButtonsLoggedOutPasswordServiceSeparator: {
+		or: "atau"
+	},
+	loginButtonsLoggedOutPasswordService: {
+		create: "Buat",
+		signIn: "Masuk",
+		forgot: "Lupa kata sandi?",
+		createAcc: "Membuat akun"
+	},
+	forgotPasswordForm: {
+		email: "Email",
+		reset: "Reset kata sandi",
+		invalidEmail: "Email salah"
+	},
+	loginButtonsBackToLoginLink: {
+		back: "Batal"
+	},
+	loginButtonsChangePassword: {
+		submit: "Ubah Password",
+		cancel: "Batal"
+	},
+	loginButtonsLoggedOutSingleLoginButton: {
+		signInWith: "Masuk menggunakan",
+		configure: "Pengaturan",
+	},
+	loginButtonsLoggedInSingleLogoutButton: {
+		signOut: "Keluar"
+	},
+	loginButtonsLoggedOut: {
+		noLoginServices: "Tidak ada layanan login dikonfigurasi"
+	},
+	loginFields: {
+		usernameOrEmail: "Nama Pengguna atau Email",
+		username: "Nama pengguna",
+		email: "Email",
+		password: "Kata sandi"
+	},
+	signupFields: {
+		username: "Nama Pengguna",
+		email: "Email",
+		emailOpt: "Email (opsional)",
+		password: "Kata Sandi",
+		passwordAgain: "Password (again)"
+	},
+	changePasswordFields: {
+		currentPassword: "Kata sandi lama",
+		newPassword: "Kata sandi baru",
+		newPasswordAgain: "Kata sandi baru (lagi)"
+	},
+	infoMessages : {
+		emailSent: "Email terkirim",
+		passwordChanged: "Kata sandi telah diubah"
+	},
+	errorMessages: {
+		genericTitle: "Ada kesalahan",
+		userNotFound: "Pengguna tidak ditemukan",
+		invalidEmail: "Email salah",
+		incorrectPassword: "Kata sandi salah",
+		usernameTooShort: "Nama pengguna harus minimal 3 karakter",
+		passwordTooShort: "Kata sandi harus minimal 3 karakter",
+		passwordsDontMatch: "Kata sandi tidak cocok",
+		newPasswordSameAsOld: "Kata sandi baru dan lama harus sama",
+		signupsForbidden: "Pendaftaran dilarang"
+	}
+});
