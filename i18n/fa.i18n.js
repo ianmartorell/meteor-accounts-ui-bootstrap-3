b2cf5dc1d07807c7643e573e2651a615143c08ff
Alex Gholamian <mm.gholamian@yahoo.com>
2017-03-27 17:27:37 +0430

    update fa.i18n.js
    Correct Persian language.

--- i18n/fa.i18n.js
+++ i18n/fa.i18n.js
@@ -1,20 +1,20 @@
 i18n.map("fa", {
 	resetPasswordDialog: {
-		title: "رمز عبور خود را تنظیم مجدد",
+		title: "تنظیم مجدد رمز عبور",
 		newPassword: "رمز عبور جدید",
-		newPasswordAgain: "رمز عبور (دوباره)",
+		newPasswordAgain: "رمز عبور (تکرار)",
 		cancel: "لغو",
 		submit: "تنظیم رمز عبور"
 	},
 	enrollAccountDialog: {
 		title: "یک رمز عبور انتخاب کنید",
 		newPassword: "رمز عبور جدید",
-		newPasswordAgain: "رمز عبور (دوباره)",
-		cancel: "نزدیک",
+		newPasswordAgain: "رمز عبور جدید (تکرار)",
+		cancel: "بستن",
 		submit: "تنظیم رمز عبور"
 	},
 	justVerifiedEmailDialog: {
-		verified: "آدرس ایمیل تایید",
+		verified: "آدرس ایمیل تایید شد",
 		dismiss: "پنهان کن"
 	},
 	loginButtonsMessagesDialog: {
@@ -26,7 +26,7 @@ i18n.map("fa", {
 	},
 	loginButtonsLoggedOutDropdown: {
 		signIn: "ورود",
-		up: "بپیوندید"
+		up: "ثبت نام"
 	},
 	loginButtonsLoggedOutPasswordServiceSeparator: {
 		or: "یا"
@@ -57,7 +57,7 @@ i18n.map("fa", {
 		signOut: "خروج"
 	},
 	loginButtonsLoggedOut: {
-		noLoginServices: "بدون خدمات پیکربندی ورود"
+		noLoginServices: "سیستم ورود تنظیم نشده است"
 	},
 	loginFields: {
 		usernameOrEmail: "نام کاربری یا پست الکترونیک",
@@ -68,29 +68,29 @@ i18n.map("fa", {
 	signupFields: {
 		username: "نام کاربری",
 		email: "ایمیل",
-		emailOpt: "ایمیل اختیاری ",
+		emailOpt: "ایمیل (اختیاری) ",
 		password: "رمز عبور",
-			passwordAgain: "رمز عبور دوباره "
+		passwordAgain: "رمز عبور (تکرار) "
 	},
-		changePasswordFields: {
+	changePasswordFields: {
 		currentPassword: "رمز عبور فعلی",
 		newPassword: "رمز عبور جدید",
-		newPasswordAgain:  "رمز عبر (دوباره"
+		newPasswordAgain:  "رمز عبور (تکرار)"
 	},
-		infoMessages : {
-		emailSent: "ایمیل ارسال",
+	infoMessages : {
+		emailSent: "ایمیل ارسال شد",
 		passwordChanged: "رمز عبور تغییر کرد"
 	},
-		errorMessages: {
-		genericTitle: "یک خطای وجود دارد",
-	userNotFound: "کاربر پیدا نشد",
-	invalidEmail: "ایمیل نامعتبر",
-	incorrectPassword: "رمز عبور اشتباه",
-	usernameTooShort: "نام کاربری حداقل باید 3 کاراکتر باشد",
-	passwordTooShort: "رمز عبور باید حداقل 6 کاراکتر باشد",
-	passwordsDontMatch: "کلمه عبور هماهنگ نیست",
-	newPasswordSameAsOld: "کلمه عبور جدید و قدیمی باید متفاوت باشد",
-	signupsForbidden: "ثبت نام ممنوع"
+	errorMessages: {
+		genericTitle: "یک خطا وجود دارد",
+		userNotFound: "کاربر پیدا نشد",
+		invalidEmail: "ایمیل نامعتبر است",
+		incorrectPassword: "رمز عبور اشتباه",
+		usernameTooShort: "نام کاربری حداقل باید 3 کاراکتر باشد",
+		passwordTooShort: "رمز عبور باید حداقل 6 کاراکتر باشد",
+		passwordsDontMatch: "کلمه عبور یکسان نیست",
+		newPasswordSameAsOld: "کلمه عبور جدید و قدیمی باید متفاوت باشد",
+		signupsForbidden: "ثبت نام ممنوع است"
 	}
 
 });
